aeec105ae9d12ecbc025d8d14e06f89a7275b97c
N-eeraj <neeraj7rajeena@gmail.com>
2021-10-23 22:53:48 +0530

    completed ui

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ $(document).ready(function () {
         $('label div').toggleClass('toggle-container-dark');
         $('label div div').toggleClass('toggle-button-dark');
         $('#calc').toggleClass('calc-dark');
+        $('#calc span:first-child').toggleClass('calc-span-first-dark');
+        $('#calc span:last-child').toggleClass('calc-span-last-dark');
+        $('button').toggleClass('button');
+        $('#del').toggleClass('del-dark');
     });
 
 
