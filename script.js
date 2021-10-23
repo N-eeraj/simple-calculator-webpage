2685fefca3f7d4f442e2020d9098099f6b207856
N-eeraj <neeraj7rajeena@gmail.com>
2021-10-23 17:01:40 +0530

    redesigning: responsive dark mode toggle

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-display = document.getElementsByTagName('h1')[0];
+$(document).ready(function () {
+    
+    $('#dark_mode').on('change', function () {
+        $('body').toggleClass('body-dark');
+        $('label').toggleClass('label-dark');
+        $('label div').toggleClass('toggle-container-dark');
+        $('label div div').toggleClass('toggle-button-dark');
+        $('#calc').toggleClass('calc-dark');
+    });
 
-function fnPress(key)
-{
-    //if display is 0 then change it to key pressed
-    if (display.innerText == 0 | display.innerText == 'Error')
-        display.innerText = key;
-    //if display is not 0 then add key pressed to current display
-    else
-        display.innerText += key;
-}
 
-function fnSolve()
-{
-    //try to evaluate with eval fn, catch by displaying error mesage
-    try{display.innerText = eval(display.innerText);}
-    catch{display.innerText = 'Error';}
-}
-
-function fnClear()
-{
-    display.innerText = '0';
-}
-
-function fnDel()
-{
-    //check if expression has only 1 character, then clear display
-    if (display.innerText.length == 1)
-        fnClear()
-    // else we set sliced value
-    else
-        display.innerText = display.innerText.slice(0, display.innerText.length - 1)
-}
+});
