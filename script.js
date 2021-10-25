748ac055b3c2b6ad3bab6cdcd6a8c12b9e16e72d
N-eeraj <neeraj7rajeena@gmail.com>
2021-10-25 23:25:56 +0530

    added script

--- script.js
+++ script.js
@@ -12,5 +12,86 @@ $(document).ready(function () {
         $('#del').toggleClass('del-dark');
     });
 
+    $('button').on('click', function () {
+        let button = $(this).text();
+        currentValue = getCurrent();
+        switch(button){
+            case 'AC':
+                clear();
+                break;
+            case '':
+                del();
+                break;
+            case '%':
+                changeInput(currentValue / 100);
+                break;
+            case '÷':
+                changeInput(currentValue + '/');
+                break;
+            case '×':
+                changeInput(currentValue + '*');
+                break;
+            case '.':
+                if (currentValue == '')
+                    currentValue = '0';
+                changeInput(currentValue + button);
+                break;
+            case '=':
+                getAns();
+                break;
+            default:
+                changeInput(currentValue + button);
+        }
+    });
 
 });
+
+const getCurrent = () => {
+    let currentValue = $('#calc span:first-child').text();
+    if (currentValue == 0 && currentValue !== '0.')
+        currentValue = '';
+    return currentValue;
+}
+
+const changeInput = input => $('#calc span:first-child').text(input);
+
+const clear = () => $('#calc span').text('0');
+
+const del = () => {
+    let currentValue = getCurrent();
+    let delVal = 0;
+    if (currentValue.length > 1)
+        delVal = currentValue.slice(0, -1);
+    changeInput(delVal);
+}
+
+const getAns = () => {
+    try {
+        ans = eval(getCurrent());
+    } catch (error) {
+        ans = 'Error';
+    }
+    $('#calc span:last-child').text(ans);
+}
+
+document.addEventListener('keydown', e => {
+    let key = e.key;
+    if (! isNaN(key) || '+-*/.'.includes(key))
+        changeInput(getCurrent() + key)
+    else{
+        switch(key.toLowerCase()){
+            case 'enter':
+                getAns();
+                break;
+            case 'backspace':
+                del();
+                break;
+            case '%':
+                changeInput(getCurrent() / 100);
+                break;
+            case 'c':
+                clear();
+                break;
+        }
+    }    
+})
